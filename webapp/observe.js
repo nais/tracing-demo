8526911bb3268bd67d25573a0f342c61add7b374
Kim Tore Jensen <kimtjen@gmail.com>
2023-03-24 15:31:37 +0100

    trace multiple calls under the same traceid

--- webapp/observe.js
+++ webapp/observe.js
@@ -3,23 +3,30 @@ import {faro} from '@grafana/faro-web-sdk';
 // get OTel trace and context APIs
 const {trace, context} = faro.api.getOTEL();
 
-// const tracer = trace.getTracer('default');
-// const span = tracer.startSpan('hello');
+const tracer = trace.getTracer('default');
+const span = tracer.startSpan("let's fibonacci this shti");
 
-// context.with(trace.setSpan(context.active(), span), () => {
-    // faro.api.pushMeasurement({
-    //     type: 'custom',
-    //     values: {
-    //         nais_tracing_answer: 42,
-    //     },
-    // });
-    //
-    fetch("/api/", {method: "POST", body: JSON.stringify({"Number": 42})}).then((response) => {
-        console.log(response)
+context.with(trace.setSpan(context.active(), span), () => {
+    faro.api.pushMeasurement({
+        type: 'custom',
+        values: {
+            nais_tracing_answer: 42,
+        },
     });
 
-    // faro.api.pushLog(['nais tracing says hello']);
+    fetch("/api/", {method: "POST", body: JSON.stringify({"Number": 5})})
+        .then((response) => response.json())
+        .then((data) => {
+            fetch("/api/", {method: "POST", body: JSON.stringify({"Number": data.Number})}).then((response) => {
+                console.log(response)
+            })
+        })
+        .catch((e) => {
+            faro.api.pushLog(`got an error: ${e}`);
+        });
 
-    // span.end();
-// });
+    faro.api.pushLog(['nais tracing says hello']);
+
+    span.end();
+});
 
