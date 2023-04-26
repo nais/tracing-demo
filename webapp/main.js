7d9b7e6114fd66cda3e5cea80a009251e35b2ae1
Kim Tore Jensen <kimtjen@gmail.com>
2023-04-26 13:25:40 +0200

    import telemetry url from environment file

--- webapp/main.js
+++ webapp/main.js
@@ -2,7 +2,7 @@ import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 const faro = initializeFaro({
-    url: 'http://localhost:12347/collect',
+    url: import.meta.env.VITE_TELEMETRY_ENDPOINT,
     app: {
         name: 'tracing-demo',
         version: '1.0.0',
