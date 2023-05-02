73a209584c5e168e35d250b4d22f13466ca26b91
Carl Hedgren <carl.hedgren@nav.no>
2023-05-02 11:02:54 +0200

    Update port numbers
    I want to harmonize the demo backends to they all work
    with the example vite app

--- express/index.js
+++ express/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require("express")
+
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
     console.log("wee /")
-    res.send("Hello World!");
+    res.send("Hello World!")
+
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
--- express/tracing.js
+++ express/tracing.js
@@ -1,22 +1,31 @@
 'use strict';
+const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const opentelemetry = require("@opentelemetry/sdk-node");
 const api = require('@opentelemetry/api');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-const traceOtlpExporter = new OTLPTraceExporter({
-});
 
-const metricOtlpExporter = new OTLPMetricExporter({
-});
+// The defaults rul works fine for this demo
+const traceOtlpExporter = new OTLPTraceExporter();
+
+// The default url works fine for this demo
+const metricOtlpExporter = new OTLPMetricExporter();
 
 const sdk = new opentelemetry.NodeSDK({
+ resource: new Resource({
+    [ SemanticResourceAttributes.SERVICE_NAME ]: "my-cool-service-name",
+    [ SemanticResourceAttributes.SERVICE_NAMESPACE ]: "Nais-namespace",
+    [ SemanticResourceAttributes.SERVICE_VERSION ]: "1.0",
+ }),
   traceExporter: traceOtlpExporter,
   metricExporter: metricOtlpExporter,
   autoDetectResources: true,
-    instrumentations: [getNodeAutoInstrumentations(), new HttpInstrumentation()],
+    instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()],
 });
 
 sdk.start()
